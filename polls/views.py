4ed37639aa8a7dbfaed30ff603a33c7a2c186286
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-02-16 13:28:43 -0600

    replace with shortcut method

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Question
 
@@ -9,13 +9,10 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-        context = {
-            "question": question
-        }
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question": question
+    }
     return render(request, "polls/detail.html", context)
 
 def results(request, question_id):
