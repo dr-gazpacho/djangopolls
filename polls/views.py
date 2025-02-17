ef6ca85b6c18ae40a423e4ef5b9da91d3101c4da
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-02-16 21:47:55 -0600

    migrate the views to use generics

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,30 @@
 from django.db.models import F
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
+from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        "question": question
-    }
-    return render(request, "polls/detail.html", context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        "question": question
-    }
-    return render(request, "polls/results.html", context)
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -43,4 +44,53 @@ def vote(request, question_id):
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
+        # args needs the , to make the argument iterable
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+# from django.db.models import F
+# from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.urls import reverse
+
+# from .models import Question, Choice
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {
+#         "question": question
+#     }
+#     return render(request, "polls/detail.html", context)
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {
+#         "question": question
+#     }
+#     return render(request, "polls/results.html", context)
+
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     context = {
+#         "question": question,
+#         "error_message": "You didn't select a choice"
+#     }
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST["choice"])
+#     except (KeyError, Choice.DoesNotExist):
+#         # redisplay question voting form
+#         return render(
+#             request,
+#             "polls/detail.html",
+#             context
+#         )
+#     else:
+#         selected_choice.votes = F("votes") + 1
+#         selected_choice.save()
+#         # args needs the , to make the argument iterable
+#         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
